6eaacce6b4dfe858654b833fa02ceec6e9567352
Priya Sharma <priyasharma40819@gmai.com>
2026-09-08 13:52:04 +0530

    apply styling

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const ejs = require("ejs");
 const Listing = require("./models/listing.js");
 const path = require("path");
 const methodOverride = require("method-override");
+const ejsMate = require("ejs-mate");
 
 const MONGO_URL = async function main() {
     await mongoose.connect(process.env.MONGO_URI);
@@ -18,7 +19,8 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({extended: true}));
 app.use(methodOverride("_method"));
-
+app.engine("ejs", ejsMate);
+app.use(express.static(path.join(__dirname, "/public")));
 app.get("/", (req, res) =>{
     res.send("Hi, I am root");
 });
