531965a362bcdee3dcd081b4402f6f90d20704e0
Priya Sharma <priyasharma40819@gmai.com>
2026-09-08 10:22:48 +0530

    create routes

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const app = express();
 const mongoose = require("mongoose");
 const ejs = require("ejs");
 const Listing = require("./models/listing.js");
+const path = require("path");
+const methodOverride = require("method-override");
 
 const MONGO_URL = async function main() {
     await mongoose.connect(process.env.MONGO_URI);
@@ -12,23 +14,75 @@ MONGO_URL()
 .then(() => {console.log("connected to db")})
 .catch(err => console.log(err));
 
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.urlencoded({extended: true}));
+app.use(methodOverride("_method"));
+
 app.get("/", (req, res) =>{
     res.send("Hi, I am root");
 });
 
-app.get("/testListing", async (req, res) => {
-    let sampleListing = new Listing({
-        title: "MY New Villa",
-        description: "By the beach",
-        price: 1200,
-        location: "Calangute, Goa",
-        country: "India"
-    });
-
-     await sampleListing.save();
-     console.log("sample was saved");
-     res.send("successful testing");
+// index route
+app.get("/listings", async (req, res) => {
+   const allListings = await Listing.find({});
+   res.render("listings/index.ejs", {allListings});
 });
+
+//new route
+app.get("/listings/new", (req, res) => {
+    res.render("listings/new.ejs");
+});
+
+// show route
+app.get("/listings/:id", async (req, res) => {
+    let {id} = req.params;
+   const listing = await Listing.findById(id);
+   res.render("listings/show.ejs", {listing});
+});
+
+//create route
+app.post("/listings", async (req, res) => {
+   const newListing =  new Listing(req.body.listing);
+   await newListing.save();
+    res.redirect("/listings");
+});
+
+//edit route
+app.get("/listings/:id/edit", async (req, res) => {
+    let {id} = req.params;
+   const listing = await Listing.findById(id);
+   res.render("listings/edit.ejs", {listing});
+});
+
+//update route
+app.put("/listings/:id", async (req,res) => {
+    let {id} = req.params;
+    await Listing.findByIdAndUpdate(id, {...req.body.listing});
+    res.redirect(`/listings/${id}`);
+});
+
+//delete route
+app.delete("/listings/:id", async (req,res) => {
+    let {id} = req.params;
+   let deletedListing = await Listing.findByIdAndDelete(id);
+   console.log(deletedListing);
+   res.redirect("/listings");
+});
+
+// app.get("/testListing", async (req, res) => {
+//     let sampleListing = new Listing({
+//         title: "MY New Villa",
+//         description: "By the beach",
+//         price: 1200,
+//         location: "Calangute, Goa",
+//         country: "India"
+//     });
+
+//      await sampleListing.save();
+//      console.log("sample was saved");
+//      res.send("successful testing");
+// });
 
 app.listen(8080, () => {
     console.log("server is listening to port 8080");
